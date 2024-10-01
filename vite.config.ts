59d42b243d86d88dc0857e355ff707909b22837e
Lyall <lyall@iriesoft.net>
2024-10-02 00:24:07 +0200

    completed demo

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,24 @@
-import { defineConfig } from 'vite';
-import { babel } from '@rollup/plugin-babel';
+import { defineConfig } from "vite";
+import { babel } from "@rollup/plugin-babel";
 
 export default defineConfig({
   build: {
     lib: {
-        entry: 'src/index.ts',
-        name: 'myLib',
-        fileName: (format) => `my-lib.${format}.js`,
+      entry: "src/index.ts",
+      name: "docxcleaner",
+      fileName: (format) => `docxcleaner.${format}.js`,
     },
     rollupOptions: {
-      input: [
-        'src/index.ts',
-      ],
+      input: ["src/index.ts"],
       plugins: [
         babel({
-          babelHelpers: 'bundled',
+          babelHelpers: "bundled",
           presets: [
             [
-              '@babel/preset-env',
+              "@babel/preset-env",
               {
                 targets: {
-                  browsers: ['ie >= 10'],
+                  browsers: ["ie >= 10"],
                 },
               },
             ],
